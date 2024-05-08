3eb22da6085bcb2cf547e16e8b4b18714fafbcdc
Berk Akkaya <berkakkaya000@gmail.com>
2024-05-08 17:02:45 +0300

    modify: choose random choice w/coroutines

--- app/src/main/java/com/berkakkaya/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/berkakkaya/rockpaperscissors/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.berkakkaya.rockpaperscissors
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-enum class MoveChoice {
+enum class Choice {
     ROCK, PAPER, SCISSORS
 }
 
@@ -26,60 +30,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun convertMoveToStr(move: MoveChoice): String {
-        return when (move) {
-            MoveChoice.ROCK -> "Taş"
-            MoveChoice.PAPER -> "Kağıt"
-            else -> "Makas"
-        }
+    fun pickChoicePressed(view: View) {
+        lifecycleScope.launch { pickChoice() }
     }
 
-    @SuppressLint("SetTextI18n")
-    fun generateAndCheckMove(playerMove: MoveChoice) {
-        val computerMove: MoveChoice = MoveChoice.entries[(0..2).random()]
-        val computerMoveStr: String = convertMoveToStr(computerMove)
-        val playerMoveStr: String = convertMoveToStr(playerMove)
+    private suspend fun pickChoice() {
+        return withContext(Dispatchers.IO) {
+            // Pick a random chocice
+            val choice: Choice = Choice.entries.random()
 
-        var resultStr: String = ""
+            // Wait for 5 secs
+            delay(5000L)
 
-        if (playerMove == computerMove) {
-            resultStr = "Berabere"
-        }
+            // Display the result in UI
+            runOnUiThread {
+                val imageView: ImageView = findViewById(R.id.imageView)
 
-        resultStr = when (playerMove) {
-            MoveChoice.ROCK -> {
-                when (computerMove) {
-                    MoveChoice.PAPER -> "Bilgisayar kazandı"
-                    else -> "Oyuncu kazandı"
-                }
-            }
-            MoveChoice.PAPER -> {
-                when (computerMove) {
-                    MoveChoice.SCISSORS -> "Bilgisayar kazandı"
-                    else -> "Oyuncu kazandı"
-                }
-            }
-            else -> {
-                when (computerMove) {
-                    MoveChoice.ROCK -> "Bilgisayar kazandı"
-                    else -> "Oyuncu kazandı"
-                }
+                imageView.setImageResource(when(choice) {
+                    Choice.ROCK -> R.drawable.rock
+                    Choice.PAPER -> R.drawable.paper
+                    else -> R.drawable.scissors
+                })
             }
         }
-
-        val textView: TextView = findViewById(R.id.textView)
-        textView.text = "Oyuncu hamlesi: $playerMoveStr | Bilgisayar hamlesi: $computerMoveStr | $resultStr"
-    }
-
-    fun rockPressed(view: View) {
-        generateAndCheckMove(MoveChoice.ROCK)
-    }
-
-    fun paperPressed(view: View) {
-        generateAndCheckMove(MoveChoice.PAPER)
-    }
-
-    fun scissorsPressed(view: View) {
-        generateAndCheckMove(MoveChoice.SCISSORS)
     }
 }
